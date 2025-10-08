c25439175a61422ca1617c8a0243a2680d91393a
Zexolver <Zexolver@Disroot.org>
2025-10-08 08:59:40 -0400

    Bringing old Stylesheets over

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+// src/main.js
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// Import your global CSS files
+import './assets/styles.css'
+import './assets/nav_styles.css'
+import './assets/transition_styles.css'
 
 const app = createApp(App)
 
