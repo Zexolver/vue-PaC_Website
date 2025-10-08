cc3f8a9800eee8c4c95ba1e2f3e62a0827b7c654
Zexolver <Zexolver@Disroot.org>
2025-10-08 09:29:53 -0400

    Add Footer & Header vue files

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer>
+        Test
+    </footer>
+</template>
+
+<style scoped>
+    /* You could move footer-specific styles from styles.css here if you wanted to. */
+</style>
