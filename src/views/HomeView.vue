730d2317c7f035e335681d5199766de2801c9d83
Zexolver <Zexolver@Disroot.org>
2025-10-08 09:43:06 -0400

    Add different 'Pages' viewsites

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="Gray_Box">
+        <h2 id="Index_EC">
+            The Awesome-sauce
+            <br>
+            Awesome Phones
+        </h2>
+        <div class="box">Box 1</div>
+        <div class="box">one</div>
+    </div>
+</template>
---
+++ src/views/LinuxPhones.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="Gray_Box">
+        <h2 id="Index_EC">
+            The Awesome-sauce
+            <br>
+            Awesome Linux Phones
+        </h2>
+        <div class="box">Linux Phones info</div>
+        <div class="box">Lorem  ipsum</div>
+    </div>
+</template>
